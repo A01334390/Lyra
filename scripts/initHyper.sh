72404f04b55fe4aef61fabcca670d64b7203d0cf
Fernando Martín García Del Angel <martingarciadelangel@me.com>
2017-10-21 23:53:04 -0500

    Fixed this shit once again

---
+++ scripts/initHyper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -v
+
+#Make directory dist
+mkdir dist
+
+#Compile the file
+composer archive create -a dist/lyra-cli.bna --sourceType dir --sourceName .
+
+#Deploy
+composer network deploy -a dist/lyra-cli.bna -p hlfv1 -i PeerAdmin -s randomString -A admin -S
+
+#Echo poop
+echo poop
